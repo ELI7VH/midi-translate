393ec3f1ae35d7e8235db4dc16610352c863bb5f
elijahlucian@gmail.com <elijahlucian@gmail.com>
2019-10-15 16:53:12 -0600

    prettierrc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const noteToString = (noteNumber) => {
-  if(noteNumber%1 !== 0) return false
+const noteToString = noteNumber => {
+  if (noteNumber % 1 !== 0) return false
 
   const notes = [
     'c',
@@ -16,11 +16,10 @@ const noteToString = (noteNumber) => {
     'b',
   ]
 
-  const octave = Math.floor((noteNumber-12) / 12)
-  return `${notes[(noteNumber % 12)]}${octave}`
+  const octave = Math.floor((noteNumber - 12) / 12)
+  return `${notes[noteNumber % 12]}${octave}`
 }
 
 const stringToNote = () => {}
-
 
 module.exports = { noteToString, stringToNote }
