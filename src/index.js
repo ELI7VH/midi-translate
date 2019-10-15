b2bd61013e8353718d5f105593e74c27b54cfe1b
eli7vh <elijahlucian@gmail.com>
2019-10-15 11:58:27 -0600

    added tests - note number to string works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-export const noteToString = () => {}
+const noteToString = (noteNumber) => {
+  if(noteNumber%1 !== 0) return false
 
-export const stringToNote = () => {}
+  const notes = [
+    'c',
+    'c#',
+    'd',
+    'd#',
+    'e',
+    'f',
+    'f#',
+    'g',
+    'g#',
+    'a',
+    'a#',
+    'b',
+  ]
+
+  const octave = Math.floor((noteNumber-12) / 12)
+  return `${notes[(noteNumber % 12)]}${octave}`
+}
+
+const stringToNote = () => {}
+
+
+module.exports = { noteToString, stringToNote }
